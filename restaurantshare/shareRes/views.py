0d2f789aa8a780a79db16bf59d57c9bbfbb42224
Ezraelyes <ryukai0927@gmail.com>
2023-07-28 01:34:26 +0900

    EDU: practicing django

--- restaurantshare/shareRes/views.py
+++ restaurantshare/shareRes/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import *
 
 # Create your views here.
 def index(request):
+    categories = Category.objects.all()
+    content = {'categories': categories}
     # return HttpResponse("index")
-    return render(request, 'shareRes/index.html')
+    return render(request, 'shareRes/index.html', content)
 
 def restaurantDetail(request):
     # return HttpResponse("restaurantDetail")
@@ -17,3 +21,10 @@ def restaurantCreate(request):
 def categoryCreate(request):
     # return HttpResponse("categoryCreate")
     return render(request, 'shareRes/categoryCreate.html')
+
+def Create_category(request):
+    category_name = request.POST['categoryName']
+    new_category = Category(category_name = category_name)
+    new_category.save()
+    return HttpResponseRedirect(reverse('index'))
+    # return HttpResponse("여기서 category Create 기능을 구현할 거야.")
